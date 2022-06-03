03e9c94a0bc9c713be992aee827e70477470c6a8
Florian Schirmer <florian.schirmer@wellsterhealth.com>
2022-06-03 08:03:50 +0200

    Split inputs on new line only

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ async function run() {
   }
   // allow commands to be separated using commas or newlines
   const separateCommands = command
-    .split(/,|\n/)
+    .split('\n')
     .map((s) => s.trim())
     .filter(Boolean)
   debug(
@@ -108,7 +108,7 @@ const startServersMaybe = () => {
 
   // allow commands to be separated using commas or newlines
   const separateStartCommands = startCommand
-    .split(/,|\n/)
+    .split('\n')
     .map((s) => s.trim())
     .filter(Boolean)
   debug(
@@ -141,7 +141,7 @@ const startServersMaybe = () => {
   const waitTimeoutMs = waitOnTimeout * 1000
 
   const waitUrls = waitOn
-    .split(',')
+    .split('\n')
     .map((s) => s.trim())
     .filter(Boolean)
   debug(`Waiting for urls ${waitUrls.join(', ')}`)
